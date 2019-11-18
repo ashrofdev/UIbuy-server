422de5cc4868bd3f3941610f7444440ad4a2976a
First Last <salmanashraf853@gmail.com>
2019-11-18 17:47:15 +0100

    I hope this works

--- server.js
+++ server.js
@@ -3,38 +3,34 @@ const nodemailer = require('nodemailer');
 const cors = require('cors')
 const app = express()
 
-app.use(cors)
+app.use(cors())
 
-app.get('/r', (req, res)=>{
-    async function main() {
-        let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 465,
-            
-            auth: {
-              user: "ashsal2001@gmail.com",
-              pass: "salmanashrafatmagul"
-            },
-            tls: {
-              
-              rejectUnauthorized: false
-            }
-          });
-    
+app.get('/', (req, res)=>{
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
         
-        let info = await transporter.sendMail({
-            from: 'ashsal2001@gmail.com', // sender address
-            to: 'ashsall115@gmail.com', // list of receivers
-            subject: 'PRODUCT REQUEST ✔', // Subject line
-            text: 'Hello world?', // plain text body
-            html: '<b>A client requested your product</b> <p>Wow, finally youve got a customer</p>' // html body
-        });
+        auth: {
+          user: "ashsal2001@gmail.com",
+          pass: "salmanashrafatmagul"
+        },
+        tls: {
+          
+          rejectUnauthorized: false
+        }
+      });
     
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    }
     
-    main().then(()=>res.json('sent')).catch((err)=>console.log(err));
+      let info = transporter.sendMail({
+          from: 'ashsal2001@gmail.com', 
+          to: 'ashsall115@gmail.com', 
+          subject: 'PRODUCT REQUEST ✔', 
+          text: 'Hello world?', 
+          html: '<b>A client requested your product</b> <p>Wow, finally youve got a customer</p>' // html body
+      }).then((e)=> res.json(e)).catch((e)=>res.json(e))
+    
+      console.log('Message sent: %s', info.messageId,);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 })
 
-app.listen(process.env.PORT || 3001)
+app.listen(process.env.PORT || 3000)
