a8e49a953339f58622e1395a8c43a4381fa6847b
First Last <salmanashraf853@gmail.com>
2019-11-20 20:57:36 +0100

    m

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 
 
 app.use((req, res, next) => {
@@ -10,6 +11,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/test', (req, res)=>{
   res.json('come on')
