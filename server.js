271c23c57996816491ddca622e6449685a5a67af
First Last <salmanashraf853@gmail.com>
2019-11-20 20:35:39 +0100

    m

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const app = express()
 
-app.use(cors())
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
