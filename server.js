9bca218fd9f58602b9da1e8a6bae23e761e34207
First Last <salmanashraf853@gmail.com>
2019-11-20 20:13:30 +0100

    v

--- server.js
+++ server.js
@@ -22,38 +22,39 @@ app.post('/send-message', (req, res)=>{
   const test = {
     name: req.body.name
   }
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    
+    auth: {
+      user: "ashsal2001@gmail.com",
+      pass: "salmanashrafatmagul"
+    },
+    tls: {
+      
+      rejectUnauthorized: false
+    }
+  });
+
+
+  let info = transporter.sendMail({
+      from: 'ashsal2001@gmail.com', 
+      to: 'ashsall115@gmail.com', 
+      subject: 'PRODUCT REQUEST ✔', 
+      text: 'Hello world?', 
+      html: '<b>A client requested your product</b> <p>Wow, finally youve got a customer</p>' // html body
+  }).then(()=> res.json({
+    name: 'ashsall115@gmail.com'
+  })).catch((e)=>res.json('err', e))
+
+  console.log('Message sent: %s', info.messageId,);
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
       res.json(test)
 })
 
 
 app.get('/', (req, res)=>{
-      let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        
-        auth: {
-          user: "ashsal2001@gmail.com",
-          pass: "salmanashrafatmagul"
-        },
-        tls: {
-          
-          rejectUnauthorized: false
-        }
-      });
-    
-    
-      let info = transporter.sendMail({
-          from: 'ashsal2001@gmail.com', 
-          to: 'ashsall115@gmail.com', 
-          subject: 'PRODUCT REQUEST ✔', 
-          text: 'Hello world?', 
-          html: '<b>A client requested your product</b> <p>Wow, finally youve got a customer</p>' // html body
-      }).then(()=> res.json({
-        name: 'ashsall115@gmail.com'
-      })).catch((e)=>res.json('err', e))
-    
-      console.log('Message sent: %s', info.messageId,);
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      
       
     
   
